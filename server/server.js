23fbe8278311b88377078bae1469f7ee1bde029d
Mike <mikeh09@gmail.com>
2018-01-23 08:30:12 -0800

    set up firebase and add create game endpoint

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
+var gameHandler = require('./handlers/game_handler');
 var app = express();
 
 app.use("/static", express.static(path.join(__dirname, "/../public")))
 
-app.get("/", function(req, res) {
+app.get("/", function(req, res, next) {
     var filePath = path.join(__dirname + "/../index.html");
     res.sendFile(filePath);
 });
+
+app.post("/api/game/create", gameHandler.createGameHandler);
 
 app.listen(8080);
